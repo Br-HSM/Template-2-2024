da75488eec95323e29a4fbbafae035844b218bb0
BrHoussem <brhhsm55@gmail.com>
2024-02-01 01:14:38 +0100

    first commit

---
+++ JS/app.js
@@ -0,0 +1,28 @@
+// Scroll Animation
+const observeEL = new IntersectionObserver((element) => {
+    element.forEach((element) => {
+       if(element.isIntersecting){
+        element.target.classList.add("show");
+       }
+       else {
+        element.target.classList.remove("show");
+       }
+    });
+});
+
+const hiddenEL = document.querySelectorAll(".hidden");
+
+hiddenEL.forEach((EL) => observeEL.observe(EL));
+// ---
+
+// burger menu
+function showSideBar() {
+    const sideBar = document.querySelector('.sideBar')
+    sideBar.style.display = 'flex'
+}
+
+function hideSideBar() {
+    const sideBar = document.querySelector('.sideBar')
+    sideBar.style.display = 'none'
+}
+// ---
